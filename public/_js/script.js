03c1c414afce91fa91fbbf4802f055d6b52f4e9b
LeoCp <leonardocolodette0@gmail.com>
2016-02-08 03:57:54 -0200

    vamos la

--- public/_js/script.js
+++ public/_js/script.js
@@ -1,2 +1,9 @@
 var app = angular.module("app", []);
-app.controller("ctrl" ,function () {});
+app.controller("ctrl" ,function ($scope,$http) {
+  $scope.verfUsuario = function () {
+    console.log($scope.usuario);
+    $http.post("/",$scope.usuario);
+  };
+
+
+});
--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-var express = require("express");
-var app = express();
-var path = __dirname + '/public/_views/';
+var express = require("express")
+,app = express()
+,path = __dirname + '/public/_views/'
+,mongojs = require('mongojs')
+,db = mongojs('dbWorkstation', ['usuarios'])
+,bodyParser = require('body-parser');
 
 app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 app.use('/public',  express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get("/",function(req,res){
   res.sendFile(path + "login.html");
+  db.usuarios.find(function (err,docs) {
+    console.log(docs);
+  });
+
 });
 
 app.get("/home",function(req,res){
   res.sendFile(path + "home.html");
+});
+
+app.post("/",function(req,res){
+    console.log((req.body).nome);
 });
 
 app.listen(3000, function () {
